e59406c64db0a8a93da0db1ff1a66c8e3a95df74
Artjom <kowalevichartem@mail.ru>
2021-03-23 01:49:45 +0300

    Initialization the 'Laboratory_Work_Three' project.

---
+++ ADiVE/Laboratory_Work_Three/Models/MainModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System;
+using Laboratory_Work_Three.Interfaces;
+
+namespace Laboratory_Work_Three.Models
+{
+    class MainModel : ModelBase
+    {
+
+    }
+}
---
+++ ADiVE/Laboratory_Work_Three/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Laboratory_Work_Three.Interfaces;
+using Laboratory_Work_Three.Models;
+using System;
+using System.Text;
+
+namespace Laboratory_Work_Three.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        MainModel Model = new MainModel();
+
+        event EventHandler RecalculateEventHandlerChanged;
+
+        public double VariableXTextBox
+        {
+            get
+            {
+                return variableX;
+            }
+            set
+            {
+                if (value != variableX)
+                {
+                    variableX = value;
+                    RecalculateEventHandlerChanged?.Invoke(this, new EventArgs());
+                    OnPropertyChanged(nameof(VariableXTextBox));
+                }
+            }
+        }
+        double variableX;
+    }
+}
